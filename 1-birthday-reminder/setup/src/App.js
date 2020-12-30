c4a063bbdb7762fd894caba67161defcda771df6
Catrina Friday <cat.friday@gmail.com>
2020-12-30 18:53:22 -0500

    done

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -12,9 +12,9 @@ function App() {
   return(
   <main>
     <section className='container'>
-    <h3>0 birthdays today</h3>
+    <h3>{people.length} birthdays today</h3>
     <List people={people}/>
-    <button onClick={() => console.log('click me')}>
+    <button onClick={() => setPeople([])}>
       clear all
     </button>
     </section>
